94a2dbcc73494746b7d7147556253bf17bacc3e9
Vutien12 <vtien7517@gmail.com>
2025-11-24 09:00:10 +0700

    update header

--- src/User/components/Header1/Header.vue
+++ src/User/components/Header1/Header.vue
@@ -6,33 +6,32 @@
     <!-- Top Bar -->
     <div class="top-bar">
       <div class="container">
-        <span class="welcome-message">Welcome to FleetCart store</span>
         <div class="top-links">
           <a href="#" class="top-link">
-            <i class="fa-regular fa-envelope"></i>
+            <i class="las la-envelope"></i>
             <span>Contact</span>
           </a>
           <div class="dropdown">
             <span class="dropdown-trigger">
-              <i class="fa-solid fa-globe"></i>
+              <i class="las la-language"></i>
               <span>English</span>
               <i class="fa-solid fa-chevron-down"></i>
             </span>
           </div>
           <div class="dropdown">
             <span class="dropdown-trigger">
-              <i class="fa-solid fa-dollar-sign"></i>
+              <i class="las la-money-bill"></i>
               <span>USD</span>
               <i class="fa-solid fa-chevron-down"></i>
             </span>
           </div>
           <!-- Login/Logout Link -->
           <router-link v-if="!isLoggedIn" to="/login" class="top-link">
-            <i class="fa-regular fa-user"></i>
+            <i class="las la-sign-in-alt"></i>
             <span>Login / Register</span>
           </router-link>
           <router-link v-else to="/account" class="top-link">
-            <i class="fa-regular fa-user"></i>
+            <i class="las la-user"></i>
             <span>Account</span>
           </router-link>
         </div>
@@ -66,22 +65,35 @@
               <i class="fa-solid fa-chevron-down"></i>
             </div>
             <button class="search-btn">
-              <i class="fa-solid fa-magnifying-glass"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none">
+                <path d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M22 22L20 20" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
             </button>
           </div>
 
           <!-- Utility Icons -->
           <div class="utility-icons">
-            <router-link to="/wishlist" class="icon-link">
-              <i class="fa-regular fa-heart"></i>
-              <span class="badge">{{ wishlistCount }}</span>
-            </router-link>
             <a href="#" class="icon-link compare-icon">
-              <i class="fa-solid fa-repeat"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path d="M3.58008 5.15991H17.4201C19.0801 5.15991 20.4201 6.49991 20.4201 8.15991V11.4799" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M6.74008 2L3.58008 5.15997L6.74008 8.32001" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M20.4201 18.84H6.58008C4.92008 18.84 3.58008 17.5 3.58008 15.84V12.52" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M17.26 21.9999L20.42 18.84L17.26 15.6799" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
               <span class="badge">0</span>
             </a>
+            <router-link to="/wishlist" class="icon-link">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path d="M12.62 20.81C12.28 20.93 11.72 20.93 11.38 20.81C8.48 19.82 2 15.69 2 8.68998C2 5.59998 4.49 3.09998 7.56 3.09998C9.38 3.09998 10.99 3.97998 12 5.33998C13.01 3.97998 14.63 3.09998 16.44 3.09998C19.51 3.09998 22 5.59998 22 8.68998C22 15.69 15.52 19.82 12.62 20.81Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
+              <span class="badge">{{ wishlistCount }}</span>
+            </router-link>
             <a href="#" class="icon-link" @click.prevent="openCart">
-              <i class="fa-solid fa-cart-shopping"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path d="M7.5 7.67001V6.70001C7.5 4.45001 9.31 2.24001 11.56 2.03001C14.24 1.77001 16.5 3.88001 16.5 6.51001V7.89001" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M9.00001 22H15C19.02 22 19.74 20.39 19.95 18.43L20.7 12.43C20.97 9.99 20.27 8 16 8H8.00001C3.73001 8 3.03001 9.99 3.30001 12.43L4.05001 18.43C4.26001 20.39 4.98001 22 9.00001 22Z" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
               <span class="badge">{{ cartCount }}</span>
             </a>
           </div>
@@ -123,7 +135,7 @@
         <!-- Desktop ALL CATEGORIES Button -->
         <button class="all-categories-btn desktop-only">
           <span>ALL CATEGORIES</span>
-          <i class="fa-solid fa-bars"></i>
+          <i class="las la-bars"></i>
         </button>
 
         <!-- Menu Content -->
@@ -131,7 +143,7 @@
           <router-link to="/" class="nav-link" @click="closeMenu">Home</router-link>
           <router-link to="/product" class="nav-link" @click="closeMenu">Shop</router-link>
           <router-link to="/blog" class="nav-link" @click="closeMenu">Blog</router-link>
-          <a href="#" class="nav-link" @click="closeMenu">Buy Now!</a>
+
         </div>
 
         <!-- More Content (Mobile Only) -->
