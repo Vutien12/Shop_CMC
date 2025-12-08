165f9f3ad90e8789da7964814693e44e4f719c1d
phungvanvu <phungvanvu0@gmail.com>
2025-12-08 15:24:17 +0700

    refactor(home): remove dynamic CSS import logic and revert to static stylesheet loading

--- src/User/screens/Home/Home.vue
+++ src/User/screens/Home/Home.vue
@@ -9,30 +9,13 @@
 
 <script setup>
 import "./Home.css"
-import { onMounted, onBeforeUnmount } from 'vue'
+
 import Footer from '../../components/Footer/Footer.vue'
 import Header from '../../components/Header1/Header.vue'
 import Chatbot from '@/User/components/Chatbot/Chatbot.vue'
 import Main from './Main.vue'
-
-// Dynamic import CSS - chỉ load khi component mount
-let styleElement = null
-
-onMounted(() => {
-  styleElement = document.createElement('link')
-  styleElement.rel = 'stylesheet'
-  styleElement.href = new URL('./Home.css', import.meta.url).href;
-  styleElement.setAttribute('data-home-css', 'true')
-  document.head.appendChild(styleElement)
-})
-
-onBeforeUnmount(() => {
-  // Remove CSS khi component unmount
-  if (styleElement && styleElement.parentNode) {
-    styleElement.parentNode.removeChild(styleElement)
-  }
-})
 </script>
+
 
 <style scoped>
 .home-page {
