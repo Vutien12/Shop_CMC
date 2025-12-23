dfb023a405192564f1cb38dc61f8fa80f6240868
Ta N N <tann2682004@gmail.com>
2025-12-23 10:00:17 +0700

    Chỉnh loading

--- src/Admin/components/LoadingDots.vue
+++ src/Admin/components/LoadingDots.vue
@@ -52,8 +52,9 @@ defineProps({
   position: absolute;
   top: 30px;
   left: 0;
+  right: 0;
+  bottom: 0;
   width: 100%;
-  min-height: 400px;
   display: flex;
   flex-direction: column;
   align-items: center;
